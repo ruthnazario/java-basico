33437aa0435ce444a8ecfae9218c082f3fa4f122
Ruth Nazário <ruthnazario01@gmail.com>
2026-07-30 23:11:38 -0300

    criacao de variaveis

--- tipos-variaveis/src/Tipos-variaveis.java
+++ tipos-variaveis/src/Tipos-variaveis.java
@@ -1,5 +1,19 @@
-public class App {
+public class Tipos_variaveis {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+System.out.println("Hello, World!");
+
+        String meuNome = "Ruth";
+        
+        double salariMinimo = 1270.50;
+        short numeroCurto = 10;
+        int numeroNormal = numeroCurto;
+        long numeroLongo = numeroNormal;
+        short numeroCurto2 = (short) numeroLongo;
+
+        int numero = 5;
+
+        System.out.print(numero);
     }
 }
